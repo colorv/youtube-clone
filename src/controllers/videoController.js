0127b695e5deb1c442432b599a2932a397115001
colorv <nexsblim@naver.com>
2022-07-26 00:37:50 +0900

    MongoDB, mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -40,19 +40,40 @@ export const watch = (req, res) => {
   return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 
+// GET, POST EditController
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = testVideos[id - 1];
-  res.render("editVideo", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("editVideo", {
+    pageTitle: `Editing: ${video.title}`,
+    video,
+  });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params; // req.params.id
   const { title } = req.body; // req.body.title
   testVideos[id - 1].title = title;
-  res.redirect(`/videos/${id}`);
+  return res.redirect(`/videos/${id}`);
+};
+
+// GET, POST Upload Controller
+export const getUpload = (req, res) => {
+  return res.render("uploadVideo", { pageTitle: "Upload" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "0 minutes ago",
+    views: 0,
+    id: testVideos.length + 1,
+  };
+  testVideos.push(newVideo);
+  return res.redirect("/");
 };
 
 // 기본적인 동작만 수행하므로 수정 해야함
 export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("Upload Video");
 export const removeVideo = (req, res) => res.send("Remove Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
   watch,
-  getEdit,
-  postEdit,
   upload,
   removeVideo,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -13,8 +15,9 @@ videoRouter.get("/:id(\\d+)", watch);
 
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 // 기본적인 동작만 수행하므로 수정 해야함
 videoRouter.get("/:id(\\d+)/remove", removeVideo);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
